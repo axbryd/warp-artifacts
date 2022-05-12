594bbc9071b4a5017e90e97b749fb2d473d0b590
Angelo Tulumello <angelo.tulumello@gmail.com>
2022-05-12 23:36:57 +0200

    Add IP address to the warp_compiler.sh and error handling

--- warp_compiler.sh
+++ warp_compiler.sh
@@ -1,2 +1,8 @@
-curl --location --request POST 'localhost:3200/compile' --form 'file=@"'$1'"' --form 'secname="'$2'"' > $1.json
-
+curl --fail --location --request POST '18.213.18.68:3200/compile' --form 'file=@"'$1'"' --form 'secname="'$2'"' > $1_MAT.json
+if [ $? -ne 0 ]; then
+    echo "-----------"
+    echo "An Error occurred"
+else
+    echo "-----------"
+    echo "Match-action table saved in $1_MAT.json"
+fi
